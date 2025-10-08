bf073fb496f46ad05d5d53d26b999527aa4100a3
viicolin <hectorvicogarcia@gmail.com>
2025-10-08 13:44:47 +0200

    Esta a medias, falta redondead

--- src/Ejercicios/PocionesMana.java
+++ src/Ejercicios/PocionesMana.java
@@ -1,7 +1,25 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class PocionesMana {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int pocionesMisiones = 5;
+
+        System.out.print("¿Cuantos hechizos gastas por batalla?");
+        int hechizosBatallas = sc.nextInt();
+
+        System.out.print("¿Cuantas batallas forman cada misión?");
+        int numeroBatallas = sc.nextInt();
+
+        System.out.print("¿Cuantas misiones hay en un nivel?");
+        int numeroMisiones = sc.nextInt();
+
+        int totalPociones = (hechizosBatallas * numeroBatallas * numeroMisiones) / pocionesMisiones;
+
+        System.out.println("Pociones necesarias: " + totalPociones);
     }
 }
