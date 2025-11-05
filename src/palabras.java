ec94faed140f815c16d9a484abbe3d52cac5a5fb
viicolin <hectorvicogarcia@gmail.com>
2025-11-05 10:53:55 +0100

    Palabras puesta y funcion para obtener la palabra random creada en palabras.java

--- src/palabras.java
+++ src/palabras.java
@@ -1,2 +1,19 @@
+import java.util.Random;
+
 public class palabras {
+    private String[] words = {
+            "gato", "perro", "computadora", "montaña", "programacion", "universo",
+            "sol", "luna", "estrella", "coche", "bicicleta", "tigre", "ventana",
+            "avion", "ciudad", "mariposa", "pirata", "magia", "espada", "dragon",
+            "castillo", "robot", "nube", "fuego", "agua", "piedra", "bosque",
+            "planeta", "aventura", "reloj", "tormenta", "vampiro", "zombi", "galaxia",
+            "murcielago", "pajaro", "elefante"
+    };
+
+    private Random random = new Random();
+
+    public String obteinRandomWord() {
+        int index = random.nextInt(words.length);
+        return words[index];
+    }
 }
