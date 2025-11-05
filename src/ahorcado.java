f8f169909c067fc1e4d5d09f5b1f0287e928ecb6
viicolin <hectorvicogarcia@gmail.com>
2025-11-05 12:15:27 +0100

    ahorcado creado y con la logica de que si falla en cada intento muestre la persona

--- src/ahorcado.java
+++ src/ahorcado.java
@@ -1,2 +1,24 @@
 public class ahorcado {
+    private final String[] drawComplet = {
+        " +---+",
+        " |   |",
+        " O   |",
+        "/|\\ |",
+        "/ \\ |",
+        "     |",
+        "========="
+    };
+
+    public void showAhorcado(int fallos){
+        for (int i = 0; i < drawComplet.length; i++){
+            if (i == 2 && fallos < 1)      System.out.println("     |");
+            else if (i == 3 && fallos < 3) System.out.println("     |");
+            else if (i == 4 && fallos < 5) System.out.println("     |");
+            else if (i == 3 && fallos == 3) System.out.println(" /|  |");
+            else if (i == 3 && fallos >= 4) System.out.println(" /|\\ |");
+            else if (i == 4 && fallos == 5) System.out.println(" /    |");
+            else if (i == 4 && fallos >= 6) System.out.println(" / \\ |");
+            else System.out.println(drawComplet[i]);
+        }
+    }
 }
